81b8feb21b3b2996056e5a7a3ce753bf9f0fca92
ホー・ミン・ヒエウ <78471723+HeyBabe-0312@users.noreply.github.com>
2022-05-27 19:50:47 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const cors = require('cors');
 
 var app = express();
 //cau hinh body parser
@@ -6,6 +7,14 @@ var app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
+app.use(cors({
+    credentials: true,
+    origin: [
+        'http://localhost:3000', //link frontend duoc phep truy cap data
+        'http://localhost:3001', 
+    ]
+}))
 
 //cac router 
 require('./src/routers/home.router')(app);
