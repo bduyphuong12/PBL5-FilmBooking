7c4a18edc60224c8380b5a01eef6cce28762c134
MinhTri111 <94583292+MinhTri111@users.noreply.github.com>
2022-06-16 15:05:16 +0700

    update history GD

---
+++ frontend/src/Page/UserAccount/setting/history/DetailHistory/DetailHistory.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import InfoHoaDon from "../InfoHoaDon/infoHoaDon";
+import "./detailhistory.scss";
+export default function DetailHistory({ showDetail, idGD, tong }) {
+  const [infoGD, setInfoGD] = useState(null);
+  useEffect(() => {
+    const getInfoGD = () => {
+      axios.get(`/ctdg/getByIdGD/${idGD}`).then((res) => {
+        setInfoGD(res.data);
+      });
+    };
+    getInfoGD();
+  }, [idGD]);
+  if (infoGD) {
+    return (
+      <div style={{ display: showDetail ? "flex" : "none" }}>
+        <div className="modal-detail">
+          <h3 style={{ color: "black" }}>CHI TIẾT HÓA ĐƠN</h3>
+          <div className="detail-up">
+            {React.Children.toArray(
+              infoGD.result.map((d) => <InfoHoaDon dataHD={d} />)
+            )}
+          </div>
+          <div>
+            <hr className="hr-class" />
+            <br />
+            <p className="detail-left">Tổng: {tong}đ</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ frontend/src/Page/UserAccount/setting/history/InfoHoaDon/infoHoaDon.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./infohoadon.scss";
+const InfoHoaDon = ({ dataHD }) => {
+  const [dataMatHang, setDataMatHang] = useState(null);
+  useEffect(() => {
+    const getMatHang = () => {
+      axios.get(`/mh/detail/${dataHD.id_hang}`).then((res) => {
+        setDataMatHang(res.data);
+      });
+    };
+    getMatHang();
+  });
+  if (dataMatHang) {
+    return (
+      <>
+        <div className="detail-up">
+          <p>
+            {dataMatHang.result[0].ten} x{dataHD.so_luong}:{" "}
+            {parseInt(dataMatHang.result[0].gia) * dataHD.so_luong}đ
+          </p>
+        </div>
+      </>
+    );
+  }
+};
+
+export default InfoHoaDon;
