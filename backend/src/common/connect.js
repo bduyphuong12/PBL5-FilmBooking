f9a43d39998519f398a8a64540535ae21abfb703
ホー・ミン・ヒエウ <oppahd96@gmail.com>
2022-06-22 17:50:29 +0700

    fix error disconnect

--- backend/src/common/connect.js
+++ backend/src/common/connect.js
@@ -1,19 +1,9 @@
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
+const pool = mysql.createPool({
     host: 'remotemysql.com',
     user: 'Lsb9R22Ws7',
     password: 'F6sLZZ3km3',
     database: 'Lsb9R22Ws7'
 });
-connection.connect(function (err, connection) {
-    if(err) console.log("ket noi that bai");
-});
-
-connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-    if (error) throw error;
-    console.log('The solution is: ', results[0].solution);
-  });
-
-
-module.exports = connection;
+pool.query('select 1 + 1', (err, rows) => { /* */ });
