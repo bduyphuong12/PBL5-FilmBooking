b651c69d64cfc96c9ce0611d4eebee3e1b3220e4
ホー・ミン・ヒエウ <oppahd96@gmail.com>
2022-06-22 17:52:06 +0700

    fix disconnect

--- backend/src/common/connect.js
+++ backend/src/common/connect.js
@@ -1,9 +1,11 @@
 var mysql = require('mysql');
 
-const pool = mysql.createPool({
+var pool = mysql.createPool({
     host: 'remotemysql.com',
     user: 'Lsb9R22Ws7',
     password: 'F6sLZZ3km3',
     database: 'Lsb9R22Ws7'
 });
 pool.query('select 1 + 1', (err, rows) => { /* */ });
+
+module.exports = pool;
